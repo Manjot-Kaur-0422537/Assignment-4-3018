0ab292bd85e8917ceb88ed2dcaf7270082356039
Manjot Kaur <mkaur31@academic.rrc.ca>
2025-11-02 21:16:57 -0600

    Add routes and controllers for user details and admin role assignment using Firebase Authentication

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,8 +1,8 @@
 import admin from "firebase-admin";
-import serviceAccount from "./assignment-4-e171d-firebase-adminsdk-fbsvc-df8c6b127d.json";
+import service_account from "../assignment-4-e171d-firebase-adminsdk-fbsvc-df8c6b127d.json";
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(service_account as admin.ServiceAccount),
 });
 
 export const auth = admin.auth();
